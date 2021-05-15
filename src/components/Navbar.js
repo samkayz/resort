062deac1650037d351c8dd556e3e2acdebbcda83
APPLE <ilemobayosamson@gmail.com>
2021-05-15 20:09:07 +0100

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import {FaAlignRight} from 'react-icons/fa';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Hello From Nav Bar
+            </div>
+        )
+    }
+}
