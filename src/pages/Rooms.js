1b1c99c6370acea3b9cdb78bb28c5d199f706ce4
APPLE <ilemobayosamson@gmail.com>
2021-05-17 13:45:43 +0100

    Single Room

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,14 +2,18 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import RoomsContainer from '../components/RoomsContainer'
 
 const Rooms = () => {
     return (
+        <>
         <Hero hero="roomsHero">
             <Banner title="our rooms">
                 <Link to="/" className="btn-primary">home</Link>
             </Banner>
         </Hero>
+        <RoomsContainer/>
+        </>
     )
 }
 export default Rooms
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,81 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { RoomContext } from '../context'
+import StyledHero from '../components/StyledHero'
+
 
 export default class SingleRoom extends Component {
+    constructor(props){
+        super(props)
+        // console.log(this.props)
+        this.state = {
+            slug:this.props.match.params.slug,
+            defaultBcg
+        };
+    }
+    static contextType = RoomContext;
+    // componentDidMount(){
+
+    // }
     render() {
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        if(!room){
+            return (
+                <div className="error">
+                    <h3>no such room could be found...</h3>
+                    <Link to='/rooms' className="btn-primary">Back to Rooms</Link>
+                </div>
+            );
+        }
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
+        const [mainImg, ...defaultImg] = images
         return (
-            <div>
-                Hello from single room page
-            </div>
+            <>
+            <StyledHero img={mainImg || this.state.defaultBcg}>
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className="btn-primary">
+                        Back to Rooms
+                    </Link>
+                </Banner>
+            </StyledHero>
+            <section className="single-room">
+                <div className="single-room-images">
+                    {defaultImg.map((item, index)=>{
+                        return(<img key={index} src={item} alt={name}/>)
+                    })}
+                </div>
+                <div className="single-room-info">
+                    <article className="desc">
+                        <h3>Details</h3>
+                        <p>{description}</p>
+                    </article>
+                    <article className="info">
+                        <h3>Info</h3>
+                        <h6>Price: ${price}</h6>
+                        <h6>Size: ${size} SQFT</h6>
+                        <h6>
+                            Max Capacity: {
+                                capacity > 1 ? `${capacity} People`: `${capacity} Person`
+                            }
+                        </h6>
+                        <h6>{pets?"Pets Allopwed": "No Pets Allowed"}</h6>
+                        <h6>{breakfast && "Free Breakfast Included"}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className="room-extras">
+                <h6>Extras</h6>
+                <ul className="extras">
+                    {extras.map((item, index)=>{
+                        return(<li key={index}>- {item}</li>)
+                    })}
+                </ul>
+            </section>
+            </>
         )
     }
 }
