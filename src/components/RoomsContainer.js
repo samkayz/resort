a4e0ddc880614d35b6ae49fbd2a343bba3431681
samkayz <ilemobayosamson@gmail.com>
2021-05-18 14:37:04 +0100

    Room Filter

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import RoomsFilter from './RoomsFilter'
 import RoomsList from './RoomsList'
+import {withRoomConsumer} from '../context'
+import Loading from './Loading'
 
-export default function RoomsContainer() {
-    return (
-        <div>
-            Hello from room container
-            <RoomsFilter/>
-            <RoomsList/>
-        </div>
-    )
+
+function RoomContainer({context}){
+    const {loading, sortedRooms, rooms} = context
+    if (loading){
+            return <Loading/>
+        }
+        return (
+                <>
+                    <RoomsFilter rooms={rooms}/>
+                    <RoomsList rooms={sortedRooms}/>
+                </>
+        )
+
 }
+
+export default withRoomConsumer(RoomContainer)
+
+// import React from 'react'
+// import RoomsFilter from './RoomsFilter'
+// import RoomsList from './RoomsList'
+// import {RoomConsumer} from '../context'
+// import Loading from './Loading'
+
+
+// export default function RoomsContainer() {
+//     return (
+//         <RoomConsumer>
+//                {value => {
+//                 const {loading, sortedRooms, rooms} = value
+//                 if (loading){
+//                     return <Loading/>
+//                 }
+//                 return (
+//                  <div>
+//                      Hello from room container
+//                      <RoomsFilter rooms={rooms}/>
+//                      <RoomsList rooms={sortedRooms}/>
+//                  </div>
+//                 )
+//             } 
+//          }
+//         </RoomConsumer>
+        
+//     )
+// }
--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Room from './Room'
 
-export default function RoomsList() {
+export default function RoomsList({rooms}) {
+    if (rooms.length === 0){
+        return (
+            <div className="empty-search">
+                unfortunately no room matched your search parameters
+            </div>
+        )
+    }
     return (
-        <div>
-            Hello from Room List
-        </div>
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {
+                    rooms.map(item => {
+                        return <Room key={item.id} room={item}/>
+                    })
+                }
+            </div>
+        </section>
     )
 }
